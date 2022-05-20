07e08e7f1827cc40bd56d5fde978356ede19c22a
Apanovitch <mc_CApanovitch23@mcjvs2.org>
2022-05-20 11:52:31 -0400

    StudyHall1

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function pageOne() {
+    location.href = "whatIsCarbon.html";
+}
+  
+function pageTwo() {
+    location.href = "whyShouldIBeWorried.html";
+}
+  
+function pageThree() {
+    location.href = "whatCanIDo.html";
+}
+
+
+function toTop() {
+  location.href = "index.html";
+}
+
